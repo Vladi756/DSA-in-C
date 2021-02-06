fccaf3f3767d3810a2baad248cd6314b85659bae
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-06 01:48:12 +0100

    Add files via upload
    Sequential search algorithm in C. It searches through each element of an array until it finds the element which the user inputted initially. If not found, the program lets the user know that their element is not present in the array. If found, the program tells the user at what position the element is located within the array.

---
+++ Sequential_Search.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int search(int arr[], int n, int x)         // Sequential Seach Algorithm
+{
+    int i;
+    for (i = 0; i < n; i++)                 // for loop checks every array element
+        if (arr[i] == x)                    // if statement determines whether the target is found or not
+            return i;                       // returns position of target
+    return -1;
+}
+
+int main(void)                              // Main Function
+{
+    int arr[] = { 4, 6, 8, 10, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);   // Initalizing all the variables
+    int x;
+
+    printf("Please input a number: ");      // Prompting user for Target
+    scanf("%d", &x);
+
+    int result = search(arr, n, x);
+    (result == -1)
+        ? printf("Element is not present in array")             // In case the target cannot be found.
+        : printf("Element is present at index %d.", result);     // If case target is found.
+    return 0;
+}
