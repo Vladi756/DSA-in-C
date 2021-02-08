e9aadb0cf9beaac09f9908e19c51b87c3835a334
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-08 01:19:44 +0100

    Sequential_Search
    The sequential search algorithm searches a given sorted array in a linear fashion in order to find the target it is looking for.

--- Sequential_Search.c
+++ Sequential_Search.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-int search(int arr[], int n, int x)         // Sequential Seach Algorithm
+int search(int arr[], int n, int x)         // Sequential Seach Algorithm.
 {
     int i;
-    for (i = 0; i < n; i++)                 // for loop checks every array element
+    for (i = 0; i < n; i++)                 // for loop checks every array element.
         if (arr[i] == x)                    // if statement determines whether the target is found or not
-            return i;                       // returns position of target
+            return i;                       // returns position of target.
     return -1;
 }
 
 int main(void)                              // Main Function
 {
-    int arr[] = { 4, 6, 8, 10, 50};
-    int n = sizeof(arr);                    // Initalizing all the variables
+    int arr[] = {4, 6, 8, 10, 50};
+    int n = sizeof(arr);                    // Initalizing all the variables.
     int x;
 
-    printf("Please input a number: ");      // Prompting user for Target
+    printf("Please input a number: ");      // Prompting user for Target.
     scanf("%d", &x);
 
     int result = search(arr, n, x);
